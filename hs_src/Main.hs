b3cdb232947190a6f8aff881f3eb17982b353181
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-02-20 14:48:45 +0900

    Smart LED pattern.

--- hs_src/Main.hs
+++ hs_src/Main.hs
@@ -4,6 +4,27 @@ import Control.Monad
 import Foreign.Ptr
 import Foreign.Storable
 
+foreign import ccall "c_extern.h Delay" c_delay :: Word32 -> IO ()
+
+gpioPtr :: Ptr Word32
+gpioPtr = odr
+  where periphBase     = nullPtr        `plusPtr` 0x40000000
+        arb2periphBase = periphBase     `plusPtr` 0x10000
+        gpioaBase      = arb2periphBase `plusPtr` 0x0800
+        odr            = gpioaBase      `plusPtr` 12
+
+gpioOut :: Word32 -> IO ()
+gpioOut v = do
+  poke gpioPtr v
+  c_delay 500000
+
+main :: IO ()
+main = forever $ do
+  gpioOut $ (1 `shiftL` 13)
+  gpioOut $ (1 `shiftL` 13) .|. (1 `shiftL` 15)
+  gpioOut $ (1 `shiftL` 15)
+  gpioOut $ 0
+
 {--
 lib/CMSIS/Core/CM3/stm32f10x.h
 define PERIPH_BASE           ((uint32_t)0x40000000) /*!< SRAM base address in
@@ -20,19 +41,3 @@ typedef struct
   __IO uint32_t LCKR;
 } GPIO_TypeDef;
 --}
-
-foreign import ccall "c_extern.h Delay" c_delay :: Word32 -> IO ()
-
-gpioPtr :: Ptr Word32
-gpioPtr = odr
-  where periphBase     = nullPtr        `plusPtr` 0x40000000
-        arb2periphBase = periphBase     `plusPtr` 0x10000
-        gpioaBase      = arb2periphBase `plusPtr` 0x0800
-        odr            = gpioaBase      `plusPtr` 12
-
-main :: IO ()
-main = forever $ do
-  poke gpioPtr (1 `shiftL` 13)
-  c_delay 500000
-  poke gpioPtr (1 `shiftL` 15)
-  c_delay 500000
