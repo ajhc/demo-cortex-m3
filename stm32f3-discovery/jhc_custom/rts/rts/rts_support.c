d11dd336b3678d648a1e1bd5e7799c5a1bdb826d
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-02-22 00:19:53 +0900

    Add jhc RTS to stm32f3-discovery.

---
+++ stm32f3-discovery/jhc_custom/rts/rts/rts_support.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#include "HsFFI.h"
+#include "rts/rts_support.h"
+#include "rts/profile.h"
+#include "rts/gc.h"
+
+jmp_buf jhc_uncaught;
+int jhc_argc;
+char **jhc_argv;
+char *jhc_progname;
+
+#ifdef __WIN32__
+A_UNUSED char *jhc_options_os =  "mingw32";
+A_UNUSED char *jhc_options_arch = "i386";
+#elif defined(__ARM_EABI__)
+A_UNUSED char *jhc_options_os =  "nds";
+A_UNUSED char *jhc_options_arch = "ARM";
+#else
+A_UNUSED char *jhc_options_os = "(unknown os)";
+A_UNUSED char *jhc_options_arch = "(unknown arch)";
+#endif
+
+void
+hs_set_argv(int argc, char *argv[])
+{
+        jhc_argc = argc - 1;
+        jhc_argv = argv + 1;
+        jhc_progname = argv[0];
+}
+
+void jhc_hs_init(void);
+
+static int hs_init_count;
+void
+hs_init(int *argc, char **argv[])
+{
+
+        if(!hs_init_count++) {
+                jhc_alloc_init();
+                jhc_hs_init();
+                hs_set_argv(*argc,*argv);
+#if JHC_isPosix
+                struct utsname jhc_utsname;
+                if(!uname(&jhc_utsname)) {
+                        jhc_options_arch = jhc_utsname.machine;
+                        jhc_options_os   = jhc_utsname.sysname;
+                }
+#endif
+        }
+}
+
+void
+hs_exit(void)
+{
+        if(!hs_init_count) {
+                abort();
+        }
+        if(!--hs_init_count) {
+                jhc_alloc_fini();
+		abort();
+        }
+}
+
+void abort() {
+	for (;;);
+}
+
+/* copy from http://ja.wikipedia.org/wiki/Memset */
+void *memset(void *str, int c, size_t num)
+{
+	unsigned char *ptr = (unsigned char *)str;
+	const unsigned char ch = c;
+
+	while(num--)
+		*ptr++ = ch;
+
+	return str;
+}
---
+++ stm32f3-discovery/jhc_custom/rts/test/stableptr_test.c
@@ -0,0 +1,41 @@
+#include "jhc_rts_header.h"
+#include "sys/queue.h"
+
+#include "seatest.h"
+
+#define SAMPLE_SPTR (sptr_t)TO_SPTR(P_VALUE,0xF0D0);
+
+int num_stableptrs(void) {
+        int count = 0;
+        struct StablePtr *sp;
+        LIST_FOREACH(sp, &root_StablePtrs, link)
+            count++;
+        return count;
+}
+
+bool in_stableptr_list(struct StablePtr *spi) {
+        int count = 0;
+        struct StablePtr *sp;
+        LIST_FOREACH(sp, &root_StablePtrs, link)
+            if (sp == spi)
+                    return true;
+        return false;
+}
+
+void stableptr_test(void) {
+        assert_int_equal(0, num_stableptrs());
+        sptr_t sptr = SAMPLE_SPTR;
+        wptr_t wptr = c_newStablePtr(sptr);
+        assert_int_equal(1, num_stableptrs());
+        assert_true(c_derefStablePtr(wptr) == sptr);
+        c_freeStablePtr(wptr);
+        assert_int_equal(0, num_stableptrs());
+}
+
+int main(int argc, const char *argv[])
+{
+        test_fixture_start();
+        run_test(stableptr_test);
+        test_fixture_end();
+        return 0;
+}
