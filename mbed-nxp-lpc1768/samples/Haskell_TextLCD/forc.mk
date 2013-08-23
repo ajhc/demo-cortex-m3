c4032d7abe519f5d6a67fdf304477e397ae54c18
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-08-23 19:55:59 +0900

    Use wait_api.

--- mbed-nxp-lpc1768/samples/Haskell_TextLCD/forc.mk
+++ mbed-nxp-lpc1768/samples/Haskell_TextLCD/forc.mk
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-PROJECT=Blink
+PROJECT=HsTextLCD
 GCC4MBED_DIR=../..
 LIBS_PREFIX=
 LIBS_SUFFIX=
