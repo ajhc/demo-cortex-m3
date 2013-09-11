3f9ce50dd863eaba678c893ba8023e6d3fbbdce6
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-09-12 00:19:55 +0900

    Control LEDs, and some tuning.

--- mbed-nxp-lpc1768/samples/Haskell_Http/hs_src/Main.hs
+++ mbed-nxp-lpc1768/samples/Haskell_Http/hs_src/Main.hs
@@ -4,10 +4,16 @@ import Foreign.Ptr
 
 import Delay
 import Gpio
+import Led
 import qualified TextLCD as LCD
 import EthernetInterface
 import TCPSocketConnection
 import ParseRss
+
+ledBar :: [Ptr GpioT] -> Int -> IO ()
+ledBar leds n = do sequence_ $ zipWith ($) acts leds
+                   delayMs 100
+  where acts = replicate n ledOn ++ replicate 4 ledOff
 
 receiveAll :: Ptr TCPSocketConnectionT -> IO [String]
 receiveAll tcp = unsafeInterleaveIO receiveAll' where
@@ -17,7 +23,7 @@ receiveAll tcp = unsafeInterleaveIO receiveAll' where
       s' <- unsafeInterleaveIO receiveAll'
       return $ s:s'
 
-slowPutstrDelay = delayMs 80
+slowPutstrDelay = delayMs 150
 
 slowPutstr lcd str = do
   let col = LCD.columns lcd
@@ -46,33 +52,42 @@ slowPutstr' lcd c str = do
   LCD.putstr lcd $ take (col - c) str
   slowPutstr' lcd (c - 1) str
 
-printRss lcd host (headline, url) = do
+printRss lcd leds host (headline, url) = do
+  ledBar leds 0
   tcp <- tcpSocketConnection_connect host 80
+  ledBar leds 1
   tcpSocketConnection_send_all tcp $ "GET " ++ url ++ " HTTP/1.0\n\n"
+  ledBar leds 2
   r <- receiveAll tcp
+  ledBar leds 3
   LCD.cls lcd
   LCD.putstr lcd $ ">> " ++ headline
   printTitle (slowPutstr lcd) r
   tcpSocketConnection_close tcp
-  delayMs 1000
+  ledBar leds 4
+  delayMs 500
   return ()
 
-tryDhcp :: LCD.LCDState -> IO Bool
-tryDhcp lcd = do
+tryDhcp lcd leds = do
+  ledBar leds 1
   LCD.cls lcd
   LCD.putstr lcd "Start >>= "
 --  ethernetDisconnect
   r1 <- ethernetInitDhcp
+  ledBar leds 2
   LCD.putstr lcd . show $ r1
   r2 <- ethernetConnect 12000
+  ledBar leds 3
   LCD.putstr lcd . show $ r2
   LCD.putstr lcd "\nIP"
   ip <- ethernetGetIpAddress
+  ledBar leds 4
   LCD.putstr lcd ip
   return $ r1 == 0 && r2 == 0
 
 main :: IO ()
 main = do
+  leds <- mapM initLed ledAll
   let p24 = pinName 2 2
       p26 = pinName 2 0
       p27 = pinName 0 11
@@ -80,12 +95,13 @@ main = do
       p29 = pinName 0 5
       p30 = pinName 0 4
   lcd <- LCD.initTextLCD p24 p26 (p27, p28, p29, p30) LCD.LCD16x2
-  tryDhcp lcd
+  tryDhcp lcd leds
   delayMs 500
   -- TCP
-  forever $ mapM_ (printRss lcd "www.reddit.com") [("Haskell", "http://www.reddit.com/r/haskell/.rss")
-                                                  ,("OCaml", "http://www.reddit.com/r/ocaml/.rss")
-                                                  ,("NetBSD", "http://www.reddit.com/r/NetBSD/.rss")
-                                                  ,("Debian", "http://www.reddit.com/r/debian/.rss")
-                                                  ,("Linux", "http://www.reddit.com/r/linux/.rss")
-                                                  ]
+  forever $ mapM_ (printRss lcd leds "www.reddit.com")
+                  [("Haskell", "http://www.reddit.com/r/haskell/.rss")
+                  ,("OCaml", "http://www.reddit.com/r/ocaml/.rss")
+                  ,("NetBSD", "http://www.reddit.com/r/NetBSD/.rss")
+                  ,("Debian", "http://www.reddit.com/r/debian/.rss")
+                  ,("Linux", "http://www.reddit.com/r/linux/.rss")
+                  ]
